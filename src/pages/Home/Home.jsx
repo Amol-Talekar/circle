12a2555b0e65f071812727e809ce5c0379e96e90
Amol-Talekar <amol11talekar@gmail.com>
2023-09-11 12:01:17 +0530

    implemented the pulsating circle animation

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+//import "./Style.css";
+import Styles from "./style.module.css";
+const Home = () => {
+  return (
+    <div className={Styles.main}>
+      <div className={Styles.box}></div>
+    </div>
+  );
+};
+
+export default Home;
